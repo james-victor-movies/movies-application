d2c790e586edc116fe5237a7f73d091c834c0ac4
VictorJP <victor.jaimes.puente@gmail.com>
2020-03-16 10:30:11 -0500

    udated tree, working on post section of app

--- src/api.js
+++ src/api.js
@@ -2,6 +2,74 @@ module.exports = {
     getMovies: () => {
         return fetch('/api/movies')
             .then(response => response.json());
-
-    }
+    },
+    // postMovie: (newMovie) => {
+    //     return fetch('/api/movies', {
+    //         method: 'POST',
+    //         headers: {
+    //             'Content-Type': 'application/json',
+    //         },
+    //         body: JSON.stringify(newMovie)
+    //     })
+    //         .then(response => response.json());
+    // },
+    postMovie: (input) => {
+        fetch('/api/movies', {
+            method: 'post',
+            headers: {
+                "Content-Type": 'application/json'
+            },
+            body: JSON.stringify(input),
+        })
+            .then(function (data) {
+                console.log('Request succeeded with JSON response', data);
+            })
+            .catch(function (error) {
+                console.log('Request failed', error);
+            });
+    },
 };
+
+
+
+
+// declare a function that pulls the date
+
+//create code that will link this to the other javascript files
+
+//create a variable that will export the user movie input to the json file?
+    //will use a filter
+
+// postMovie: (newMovie) => {
+//     return fetch ({title},{rating}{
+//     method: 'POST',
+//     headers: {
+//         'Content-Type': 'application/json',
+//     },
+//     body: JSON.stringify(newMovie),
+// });
+//     .then(/* post was created successfully */)
+//     .catch(/* handle errors */);
+//
+
+
+// const movieDetails = {title, rating};
+
+// postMovies: (newMovie) = {
+//     method: 'POST',
+//     headers: {
+//         'Content-Type': 'application/json',
+//     },
+//     body: JSON.stringify(movieDetails),
+// };
+
+// postMovie: (newMovie) => {
+// //     return fetch('api/movies', {
+// //         method: 'POST',
+// //         headers: {
+// //             'Content-Type': 'application/json',
+// //         },
+// //         body: JSON.stringify(newMovie)
+// //     })
+// //         .then(response => response.json());
+// // },
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 /**
  * es6 modules and imports
- *
  */
+const {postMovie} = require('./api.js');
 const $ = require('jquery');
 import sayHello from './hello';
 
@@ -19,16 +19,42 @@ getMovies().then((movies) => {
     });
     $('.container').html(`Here are the all the movies:<br> ${bucket}`)
 }).catch((error) => {
-    console.log('Oh no! Something went wrong.\nCheck the console for details.')
+    console.log('Oh no! Something went wrong.\nCheck the console for details.');
     console.log(error);
 });
 // LOADING.MESSAGE
 $(document).ready(function () {
-    $('#loadingM').text('Loading...');
+    $('#loadingM').text('James...');
 });
 //TEXT.BOX
+//FORM.DIV
+
+//1. associate button with input text (james)
+    // take variable userMovieInput and make it argument for postMovie.
+    // pass it through postMovie function and verify outcome
+
+    //1.1 modifiy the api       ( still working on this )
+//2. append to json file        ( still working on this )
+// const {postMovie} = require('./api.js');
+
+
+//this is the click function that manipulates the user input.
+//
+
+
+$('#addMovieButton').click(function() {
+    //userMovieInput variable is supposed to convert the user input to a value
+    let userMovieInput = $('#addMovieText').val();
+    //this line appends the data to the html page at the div addMovieForm
+
+     return postMovie(userMovieInput);
+});
+// postMovie('test');
+
+// });
+//addMovie
 //will target id of 
-$('')
+// $('')
 //GENERAL.STYLING
-$('#addMovieForm').css('margin', 0);
-$('.container').css('maring-top', '10px');
+// $('#addMovieForm').css('margin', 0);
+// $('.container').css('maring-top', '10px');
